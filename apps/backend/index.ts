b9f8221d75666350a331df8d67f92a747101de0a
ashish <ashishkushwaha472004@gmail.com>
2025-10-28 14:02:55 +0530

    completed the schema, added route

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
-
+import cors from "cors"
+import { prismaClient } from "db/client";
+import { auth } from "./middleware/auth";
 
 const PORT = 8080;
 
 const app = express();
 
 
+app.post("/signin", (req, res) => {
 
+})
+
+app.get("/calendar", auth, (req, res) => {
+  
+})
 
 
 app.listen(PORT, () => {
---
+++ apps/backend/middleware/auth.ts
@@ -0,0 +1,8 @@
+import type { NextFunction, Request, Response } from "express";
+
+
+
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+
+}
